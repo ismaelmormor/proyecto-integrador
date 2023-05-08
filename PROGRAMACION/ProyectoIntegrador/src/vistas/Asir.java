363145ce9c45a5ae5561386d60fed39146902238
[Santiago Daza] <[santiagodaza22@gmail.com]>
2023-05-08 09:26:52 +0200

    ASIR

--- PROGRAMACION/ProyectoIntegrador/src/vistas/Asir.java
+++ PROGRAMACION/ProyectoIntegrador/src/vistas/Asir.java
@@ -1,6 +1,7 @@
 package vistas;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.Image;
 
 import javax.swing.*;
@@ -165,6 +166,22 @@ public class Asir extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+
+		 // JLabel centrado con el texto especificado
+		 JLabel texto = new JLabel("<html>Codigo de area: 003<br></br><br></br>Descripcion:<br></br>La administración de sistemas informáticos en red implica organizar, instalar y mantener en funcionamiento los sistemas informáticos de una empresa. Esto incluye las redes de área local (LAN), las redes de área amplia (WAN), los segmentos de red, las intranets y otros</html>");
+		 texto.setFont(new Font("Arial", Font.PLAIN, 16));
+		 texto.setHorizontalAlignment(JLabel.CENTER);
+		 texto.setForeground(Color.white);
+		 texto.setBounds(50, 50, 650, 250);
+		 contentPane.add(texto);
+
+		 //label asir
+		 JLabel labelASIR = new JLabel("DAW");
+		 labelASIR.setFont(new Font("Arial", Font.BOLD, 30));
+		 labelASIR.setHorizontalAlignment(JLabel.CENTER);
+		 labelASIR.setForeground(Color.WHITE);
+		 labelASIR.setBounds(50, 50, 650, 30);
+		 contentPane.add(labelASIR);
 	}
 
 }

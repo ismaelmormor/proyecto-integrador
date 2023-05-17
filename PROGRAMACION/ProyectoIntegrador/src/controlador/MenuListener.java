1824c32c57218ced26d6d5b0715ac7637b659c5e
ismaelmormor <ismaelmormor@gmail.com>
2023-05-17 11:25:32 +0200

    comentada la clase de menuListener

--- PROGRAMACION/ProyectoIntegrador/src/controlador/MenuListener.java
+++ PROGRAMACION/ProyectoIntegrador/src/controlador/MenuListener.java
@@ -1,17 +1,20 @@
 package controlador;
-
+// Import de todo
 import vistas.*;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
+import java.awt.event.*;
+/*
+ * Clase del listener que hace funcionar el menú de cada ventana
+ */
 public class MenuListener implements ActionListener{
-
+    // Variables
     private JFrame ventana;
 
+    /* 
+     * Método constructor
+     * @param ventana - JFrame de la ventana desde donde se acciona el Listener
+     */
 	public MenuListener(JFrame ventana){
         this.ventana=ventana;
 	}
@@ -19,47 +22,69 @@ public class MenuListener implements ActionListener{
     @Override
 	public void actionPerformed(ActionEvent ev) {
 		if (ev.getSource() instanceof JMenuItem) {
+            // ActionListener para el apartado de Consulta de Proyecto Integrador
 			if (ev.getActionCommand().equals("Consulta")) {
+                //Cerramos ventana y creamos la de consulta
 				ventana.dispose();
                 ConsultaW ventanaNueva = new ConsultaW();
 		        ventanaNueva.setVisible(true);
-            }else if(ev.getActionCommand().equals("Alta")){
+            }
+            // ActionListener para el apartado de Alta de Proyecto Integrador
+            else if(ev.getActionCommand().equals("Alta")){
+                //Cerramos ventana y creamos la de Alta
                 ventana.dispose();
                 Alta ventanaNueva = new Alta();
 		        ventanaNueva.setVisible(true);
-            }else if(ev.getActionCommand().equals("Lista de Alumnos")){
+            }
+            // ActionListener para el apartado de Lista de Alumnos
+            else if(ev.getActionCommand().equals("Lista de Alumnos")){
+                //Cerramos ventana y creamos la de Lista de ALumno
                 ventana.dispose();
                 ListaDeAlumnos ventanaNueva = new ListaDeAlumnos();
 		        ventanaNueva.setVisible(true);
-            }else if(ev.getActionCommand().equals("Añadir")){
+            }
+            // ActionListener para el apartado de Añadir Alumnos
+            else if(ev.getActionCommand().equals("Añadir")){
+                //Cerramos ventana y creamos la de añadir Alumno
                 ventana.dispose();
                 Add_Alumno ventanaNueva = new Add_Alumno();
 		        ventanaNueva.setVisible(true);
-            }else if(ev.getActionCommand().equals("DAW")){
+            }
+            // ActionListener para el apartado de DAW
+            else if(ev.getActionCommand().equals("DAW")){
+                //Cerramos ventana y creamos la de DAW
                 ventana.dispose();
                 DAW ventanaNueva = new DAW();
 		        ventanaNueva.setVisible(true);
-            }else if(ev.getActionCommand().equals("DAM")){
+            }
+            // ActionListener para el apartado de DAM
+            else if(ev.getActionCommand().equals("DAM")){
+                //Cerramos ventana y creamos la de DAM
                 ventana.dispose();
                 DAM ventanaNueva = new DAM();
 		        ventanaNueva.setVisible(true);
-            }else if(ev.getActionCommand().equals("ASIR")){
+            }
+            // ActionListener para el apartado de Asir
+            else if(ev.getActionCommand().equals("ASIR")){
+                //Cerramos ventana y creamos la de Asir
                 ventana.dispose();
                 Asir ventanaNueva = new Asir();
 		        ventanaNueva.setVisible(true);
-            }else if(ev.getActionCommand().equals("ANIMACIONES 3D")){
+            }
+            // ActionListener para el apartado de Animaciones 3D
+            else if(ev.getActionCommand().equals("ANIMACIONES 3D")){
+                //Cerramos ventana y creamos la de Animaciones 3D
                 ventana.dispose();
                 Animacion3D ventanaNueva = new Animacion3D();
 		        ventanaNueva.setVisible(true);
-            }else if(ev.getActionCommand().equals("VIDEOJUEGOS")){
+            }
+            // ActionListener para el apartado de Videjuegos
+            else if(ev.getActionCommand().equals("VIDEOJUEGOS")){
+                //Cerramos ventana y creamos la de videojuegos
                 ventana.dispose();
                 Juegos ventanaNueva = new Juegos();
 		        ventanaNueva.setVisible(true);
             }
-            
-            
 		}
-
 	}
-
 }

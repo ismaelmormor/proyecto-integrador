22fcd11b28d15be226e515281679948722085f7c
ismaelmormor <ismaelmormor@gmail.com>
2023-05-17 11:31:44 +0200

    appGráfica comentada

--- PROGRAMACION/ProyectoIntegrador/src/main/AppGrafica.java
+++ PROGRAMACION/ProyectoIntegrador/src/main/AppGrafica.java
@@ -1,24 +1,24 @@
 package main;
 
-import java.awt.EventQueue;
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.*;
 
 import vistas.VentanaPrincipal;
-
+/*
+ * Creación de la clase main para ejecutar la aplicación
+ */
 public class AppGrafica {
 
 	public static void main(String[] args) { //s
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					// Creamos y hacemos visible la ventana principal de la aplicación
 					VentanaPrincipal frame = new VentanaPrincipal();
-					frame.hacerVisible();;
+					frame.hacerVisible();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-
 }

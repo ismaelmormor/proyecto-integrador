e25526e83c6dd3353930307f782ff0b3a4f56bdd
xtrerw <miguelxu124@gmail.com>
2023-05-19 11:42:47 +0200

    ROOT

--- PROGRAMACION/ProyectoIntegrador/src/modelo/AccesoBD.java
+++ PROGRAMACION/ProyectoIntegrador/src/modelo/AccesoBD.java
@@ -6,21 +6,21 @@ public class AccesoBD {
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://localhost/UF7";
 	private String usuario = "root";
-	private String pword = "SkillMoreno";
-	
+	private String pword = "root";
+
 	public Connection getConexion() {
 		Connection con = null;
 		try {
 
 			Class.forName(driver);
-	        con=DriverManager.getConnection(url,usuario,pword);
-	        System.out.println("Conexion perfecta");
-            
-	    } catch (Exception e) {
-	        System.out.println("error al establecer conexión con BD"+ e.getMessage());
-	        // TODO: handle exception
-	    }
+			con = DriverManager.getConnection(url, usuario, pword);
+			System.out.println("Conexion perfecta");
+
+		} catch (Exception e) {
+			System.out.println("error al establecer conexión con BD" + e.getMessage());
+			// TODO: handle exception
+		}
 		return con;
 	}
-	
+
 }

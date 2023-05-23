53c1ae952dfa887c7dfb9b43b71c386b9d35cc00
[Santiago Daza] <[santiagodaza22@gmail.com]>
2023-05-23 11:44:31 +0200

    login improved

--- PROGRAMACION/ProyectoIntegrador/src/vistas/VentanaLogin.java
+++ PROGRAMACION/ProyectoIntegrador/src/vistas/VentanaLogin.java
@@ -15,38 +15,51 @@ public class VentanaLogin extends JFrame {
 	public VentanaLogin() {
 		getContentPane().setLayout(null);
 		setLocationRelativeTo(null);
+
 		
-		JLabel lblNewLabel = new JLabel("usuario");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel.setBounds(203, 44, 98, 45);
-		getContentPane().add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("contraseña");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1.setBounds(203, 144, 112, 45);
-		getContentPane().add(lblNewLabel_1);
-		
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		textField.setBounds(362, 52, 130, 29);
-		getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		passwordField = new JPasswordField();
-		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		passwordField.setBounds(362, 152, 130, 29);
-		getContentPane().add(passwordField);
-		
-		final JLabel informacion = new JLabel("");
-		informacion.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		informacion.setBounds(116, 290, 540, 173);
-		getContentPane().add(informacion);
-		
-		JButton btnNewButton = new JButton("Login");
-		btnNewButton.setBounds(362, 300, 130, 29);
-		loginButton loginBtn = new loginButton(this);
-		btnNewButton.addActionListener(loginBtn);
-		getContentPane().add(btnNewButton);
+		ImageIcon logo = new ImageIcon("proyecto-integrador/PROGRAMACION/ProyectoIntegrador/img/logo.png");
+	        Image originalImage = logo.getImage();
+	        Image resizedImage = originalImage.getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+	        ImageIcon resizedIcon = new ImageIcon(resizedImage);
+	        
+	        JLabel imageLabel = new JLabel(resizedIcon);
+	        imageLabel.setBounds(367, 51, 50, 50);
+	        getContentPane().add(imageLabel);
+
+			
+			JLabel lblNewLabel = new JLabel("usuario");
+			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+			lblNewLabel.setBounds(200, 128, 98, 45);
+			getContentPane().add(lblNewLabel);
+			
+			JLabel lblNewLabel_1 = new JLabel("contraseña");
+			lblNewLabel_1.setBackground(new Color(255, 255, 255));
+			lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+			lblNewLabel_1.setBounds(176, 219, 112, 45);
+			getContentPane().add(lblNewLabel_1);
+			
+			textField = new JTextField();
+			textField.setFont(new Font("Tahoma", Font.PLAIN, 20));
+			textField.setBounds(334, 136, 130, 29);
+			getContentPane().add(textField);
+			textField.setColumns(10);
+			
+			passwordField = new JPasswordField();
+			passwordField.setFont(new Font("Tahoma", Font.PLAIN, 20));
+			passwordField.setBounds(334, 227, 130, 29);
+			getContentPane().add(passwordField);
+			
+			final JLabel informacion = new JLabel("");
+			informacion.setFont(new Font("Tahoma", Font.PLAIN, 20));
+			informacion.setBounds(48, 290, 540, 173);
+			getContentPane().add(informacion);
+			
+			JButton btnNewButton = new JButton("Login");
+			btnNewButton.setBounds(334, 335, 130, 29);
+			loginButton loginBtn = new loginButton(this);
+			btnNewButton.addActionListener(loginBtn);
+			getContentPane().add(btnNewButton);
 
 		setSize(800, 500);
 		setLocationRelativeTo(null);

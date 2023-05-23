080247c81af7f28c2b118974c82910801f67ba73
ismaelmormor <ismaelmormor@gmail.com>
2023-05-23 22:34:00 +0200

    Cambios en áreas + base de datos

--- PROGRAMACION/ProyectoIntegrador/src/vistas/alumno/DAMA.java
+++ PROGRAMACION/ProyectoIntegrador/src/vistas/alumno/DAMA.java
@@ -1,24 +1,19 @@
 package vistas.alumno;
 
-import java.awt.Font;
-import java.awt.Image;
+import java.awt.*;
+import java.sql.*;
 
 import javax.swing.*;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import controlador.alumno.MenuListenerA;
-
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import java.awt.Color;
+import modelo.AccesoBD;
 
 public class DAMA extends JFrame {
 
 	private JPanel contentPane;
+	private String descripcion, nombre;
+	private Connection con;
 	private MenuListenerA menuListener = new MenuListenerA(this);
 
 	/**
@@ -105,20 +100,36 @@ public class DAMA extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-        // JLabel centrado con el texto especificado
-        JLabel texto = new JLabel("<html>Codigo de area: 002<br></br><br></br>Descripcion:<br></br>El desarrollo de aplicaciones multiplataforma es una estrategia que utiliza software para crear aplicaciones para las plataformas iOS y Android simultáneamente. Esto permite a las empresas llegar a una base de clientes más amplia proporcionándoles una aplicación compatible con varios dispositivos.</html>");
-		texto.setFont(new Font("Arial", Font.PLAIN, 20));
-		texto.setHorizontalAlignment(JLabel.CENTER);
-		texto.setForeground(Color.white);
-		texto.setBounds(163, 228, 650, 250);
-        contentPane.add(texto);
-        //label dam
-        JLabel labelDAM = new JLabel("DAM");
-        labelDAM.setFont(new Font("Arial", Font.BOLD, 35));
-		labelDAM.setHorizontalAlignment(JLabel.CENTER);
-	    labelDAM.setForeground(Color.WHITE);
-		labelDAM.setBounds(138, 111, 650, 30);
-        contentPane.add(labelDAM);
+        AccesoBD access = new AccesoBD();
+		con = access.getConexion();
+
+		try {
+			// Preparamos la consulta a la base de datos
+			Statement statement = con.createStatement();
+			String query = "select * from AREAS where ID_AREA=2";
+			ResultSet result = statement.executeQuery(query);
+			nombre = result.getString("Nombre");
+			descripcion = result.getString("Descripcion");
+		} catch (Exception e) {
+			System.out.println("Hubo un error: "+e.getMessage());
+			// TODO: handle exception
+		}
+
+		 // JLabel centrado con el texto especificado
+		 JLabel texto = new JLabel("<html>Codigo de area: 002<br></br><br></br>Descripcion:<br></br>"+descripcion+"</html>");
+		 texto.setFont(new Font("Arial", Font.PLAIN, 20));
+		 texto.setHorizontalAlignment(JLabel.CENTER);
+		 texto.setForeground(Color.white);
+		 texto.setBounds(163, 228, 650, 250);
+		 contentPane.add(texto);
+
+		 //label ANIMACION
+		 JLabel label3D = new JLabel(nombre);
+		 label3D.setFont(new Font("Arial", Font.BOLD, 35));
+		 label3D.setHorizontalAlignment(JLabel.CENTER);
+	     label3D.setForeground(Color.WHITE);
+		 label3D.setBounds(138, 111, 650, 30);
+		 contentPane.add(label3D);
 	}
 
 }

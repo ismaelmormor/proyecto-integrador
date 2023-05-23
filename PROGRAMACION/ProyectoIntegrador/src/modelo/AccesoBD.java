ed7c0bf2b6beb0fcdb88a21026ca5150e04faad9
ismaelmormor <ismaelmormor@gmail.com>
2023-05-23 23:58:47 +0200

    Comentario javadoc en modelo

--- PROGRAMACION/ProyectoIntegrador/src/modelo/AccesoBD.java
+++ PROGRAMACION/ProyectoIntegrador/src/modelo/AccesoBD.java
@@ -1,17 +1,21 @@
 package modelo;
 
 import java.sql.*;
-
+/**
+ * Clase para realizar la conexión con la base de datos
+ */
 public class AccesoBD {
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://localhost/ProyectoIntegrador";
 	private String usuario = "root";
-	private String pword = "SkillMoreno";
-
+	private String pword = "root";
+	/**
+	 * Método para obtener la conexión
+	 * @return con - Devuelve la conexión con la base de datos
+	 */
 	public Connection getConexion() {
 		Connection con = null;
 		try {
-
 			Class.forName(driver);
 			con = DriverManager.getConnection(url, usuario, pword);
 			System.out.println("Conexion perfecta");
